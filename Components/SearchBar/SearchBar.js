45a76e2d55cd0f8b0ee20198d4ad7cce57ad2182
shivendrarox <32977980+shivendrarox@users.noreply.github.com>
2022-05-21 17:12:22 +0530

    adding handlers

--- Components/SearchBar/SearchBar.js
+++ Components/SearchBar/SearchBar.js
@@ -1,18 +1,38 @@
 import {useState} from 'react'
 import styles from "./styles.module.scss"
-// import axios from "axios"
+import axios from "axios"
 const SearhBar  = ({placeholder,inputData}) => {
     const [filteredRepos,setFilteredRepos] = useState([])
     const [userInput,setUserInput] = useState("")
 
-    const handleFilter = (event)=>{
+    const handleFilter = async (event)=>{
         const searchTerm = event.target.value
         setUserInput(searchTerm)
-
-        const filterRepos = data.filter( async (value)=>{
-            // let dataFromGh = await axios.get("")
-        })
+        try{
+            let dataFromGh = await axios.get(`https://api.github.com/search/repositories?q=mozilla&per_page=10`)
+            const newFilteredRepos = dataFromGh.data.items.filter((value)=>{
+                return value.name.toLowerCase().includes(searchTerm.toLowerCase());
+            })
+            if (searchTerm === "") {
+                setFilteredRepos([]);
+              } else {
+                setFilteredRepos(newFilteredRepos);
+              }
+        }
+        catch(error){
+            console.log(error)
+        }
+   
     }
+
+  const resetSearchBar = () =>{
+    setFilteredRepos([])
+    setUserInput("")
+  }
+
+  return(<>
+  
+  </>)
 }
 
 export default SearhBar
